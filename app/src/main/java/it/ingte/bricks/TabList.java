92eed8a63331a71e27e0d59d7a1ac6e8013938c7
Davide Bassetto <biase996@gmail.com>
2017-12-17 19:34:35 +0100

    Add gestione Tab (NB: crash al tocco su LIST o allo slide tra i tab)

---
+++ app/src/main/java/it/ingte/bricks/TabList.java
@@ -0,0 +1,22 @@
+package it.ingte.bricks;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by davide on 17/12/2017.
+ */
+
+public class TabList extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab_list, container, false);
+        return rootView;
+    }
+
+}
