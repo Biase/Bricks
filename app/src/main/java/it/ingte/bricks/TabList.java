193ca23e979e35f0986d92f933f766279bfe00a3
Davide Bassetto <biase996@gmail.com>
2017-12-20 15:59:45 +0100

    Implement static List

--- app/src/main/java/it/ingte/bricks/TabList.java
+++ app/src/main/java/it/ingte/bricks/TabList.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by davide on 17/12/2017.
@@ -12,11 +14,28 @@ import android.view.ViewGroup;
 
 public class TabList extends Fragment {
 
+    String[] lstSource = {
+            "Jack Daniel's",
+            "Absolute",
+            "Belvedere",
+            "Greygoose",
+            "Bulldog",
+            "Bombay"
+    };
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tab_list, container, false);
+        perform(rootView);
+
         return rootView;
+    }
+
+    public void perform(View v) {
+        ListView lst = v.findViewById(R.id.lstView);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, lstSource);
+        lst.setAdapter(adapter);
     }
 
 }
