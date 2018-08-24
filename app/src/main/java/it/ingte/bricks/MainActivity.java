fab3cc27686a7664c225953f1d6ea9c89eaf8dc1
MimmoPeris <857273@stud.unive.it>
2018-08-24 23:31:36 +0200

    APP NUOVA aggiunta nuova activity per beneficiario sistemazione somma totale e non media nome bricks collegamento Tab_map a grafico messa a punto search tab_beneficiary FINITO TASBORO

--- app/src/main/java/it/ingte/bricks/MainActivity.java
+++ app/src/main/java/it/ingte/bricks/MainActivity.java
@@ -50,14 +50,14 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Bricks");
+        getSupportActionBar().setTitle("FSE_Bricks");
 
 
         // Create the adapter that will return a fragment for each of the three (two in our case)
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        
+
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
